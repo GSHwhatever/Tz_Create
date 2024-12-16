26ea94d8c96665a37f4772c61d277bc8d9d4ffb6
GSH <422455159@qq.com>
2024-12-16 20:57:13 +0800

    修改生成统计报表功能，及台账细节

--- Write_BB.py
+++ Write_BB.py
@@ -9,11 +9,10 @@ import os
 class Write:
 
     def __init__(self) -> None:
-        self.smz_path = r'D:/weixin/Other/小半道社区7月新就业人员实名制.xlsx'
+        self.smz_path = r'C:/Users/GSH\Desktop/小半道社区2024实名制.xlsx'
         out_path = os.path.join(os.environ['USERPROFILE'], 'Desktop', '台账结果', '就业统计报表.xlsx')
         self.bb_path = out_path if os.path.exists(out_path) else os.path.join(os.path.dirname(__file__), 'template_excel', '就业统计报表.xlsx')
         self.out_path = os.path.join(os.environ['USERPROFILE'], 'Desktop', '就业统计报表.xlsx')
-        self.row = datetime.now().month + 5
         self.industries = [
             "农、林、牧、渔业",
             "采矿业",
@@ -41,32 +40,39 @@ class Write:
         self.sy02 = []
         self.hyhf = []
     
-    def read(self):
+    def re_init(self):
+        self.EP1 = []
+        self.sjy01 = []
+        self.sy02 = []
+        self.hyhf = []
+    
+    def read(self, month : int):
         smz_wb = load_workbook(self.smz_path)
         all_ws = smz_wb['新就业人员']
         xzjy_ws = smz_wb['新增就业人员']
         zrjy_ws = smz_wb['自然减员（就业）']
         syry_ws = smz_wb['失业人员情况']
 
-        xzjy_num = len([i for i in xzjy_ws['A'] if i.value]) - 2        # 新增就业人数
-        zrjy_num = len([i for i in zrjy_ws['A'] if i.value]) - 2        # 自然减员人数
+        # print([v.value for i, v in zip(zrjy_ws['A'], zrjy_ws['I']) if (i.value and isinstance(v.value, datetime)) and v.value.month <= datetime.now().month])
+        xzjy_num = len([i for i, v in zip(xzjy_ws['A'], xzjy_ws['J'])  if (i.value and isinstance(v.value, datetime)) and v.value.month <= month])        # 新增就业人数
+        zrjy_num = len([i for i, v in zip(zrjy_ws['A'], zrjy_ws['I']) if (i.value and isinstance(v.value, datetime)) and v.value.month <= month])        # 自然减员人数
         
-        syzjy_num = len([i for i in all_ws['K'] if i.value and i.value == '失业再就业'])        # 失业再就业人数
-        jykn_num = len([i for i in all_ws['R'] if i.value and i.value == '是'])     # 就业困难人数
+        syzjy_num = len([i for i, v in zip(all_ws['K'], all_ws['H']) if i.value and i.value == '失业再就业' and isinstance(v.value, datetime) and v.value.month <= month])        # 失业再就业人数
+        jykn_num = len([i for i, v in zip(all_ws['R'], all_ws['H']) if i.value and i.value == '是' and isinstance(v.value, datetime) and v.value.month <= month])     # 就业困难人数
 
-        xl_num = len([i for i in all_ws['G'] if i.value and i.value in ('大学专科', '大学本科', '硕士研究生')])     # 大专以上学历人数
-        nv_num = len([i for i in all_ws['X'] if i.value and i.value == '女'])       # 女性人数
-        cylb_dic = Counter([i.value for i in all_ws['P'] if i.value and i.value in ('第一产业', '第二产业', '第三产业')])     # 产业类别计数
-        jyqd_dic = Counter([i.value for i in all_ws['J'] if i.value and i.value in ('单位就业', '灵活就业', '个体工商户', '公益性岗位安置')])     # 就业渠道计数
-        xydm_lis = [i.value for i, j in zip(all_ws['L'], all_ws['J']) if j == '单位就业' and i.value and i.value.isalnum()]      # 单位就业人员单位统一就业代码
+        xl_num = len([i for i, v in zip(all_ws['G'], all_ws['H']) if i.value and i.value in ('大学专科', '大学本科', '硕士研究生') and isinstance(v.value, datetime) and v.value.month <= month])     # 大专以上学历人数
+        nv_num = len([i for i, v in zip(all_ws['X'], all_ws['H']) if i.value and i.value == '女' and isinstance(v.value, datetime) and v.value.month <= month])       # 女性人数
+        cylb_dic = Counter([i.value for i, v in zip(all_ws['P'], all_ws['H']) if i.value and i.value in ('第一产业', '第二产业', '第三产业') and isinstance(v.value, datetime) and v.value.month <= month])     # 产业类别计数
+        jyqd_dic = Counter([i.value for i, v in zip(all_ws['J'], all_ws['H']) if i.value and i.value in ('单位就业', '灵活就业', '个体工商户', '公益性岗位安置') and isinstance(v.value, datetime) and v.value.month <= month])     # 就业渠道计数
+        xydm_lis = [i.value for i, j, v in zip(all_ws['L'], all_ws['J'], all_ws['H']) if j == '单位就业' and i.value and i.value.isalnum() and isinstance(v.value, datetime) and v.value.month <= month]      # 单位就业人员单位统一就业代码
         sydw_num = len([i for i in xydm_lis if i[0] == '1'])      # 根据统一信用代码筛选第一位为'1'的为机关事业单位
-        nlhf_dic = Counter([i.value for i in all_ws['AF'] if i.value and i.value in ('16-24', '25-45', '46-60')])     # 年龄划分计数
+        nlhf_dic = Counter([i.value for i, v in zip(all_ws['AF'], all_ws['H']) if i.value and i.value in ('16-24', '25-45', '46-60') and isinstance(v.value, datetime) and v.value.month <= month])     # 年龄划分计数
 
         syry_num = len([i for i in syry_ws['A'] if i.value]) - 2        # 失业人员人数
         synx_num = len([i for i in syry_ws['C'] if i.value and i.value == '女'])        # 失业人员其中女性人数
         sykn_num = len([i for i in syry_ws['K'] if i.value and i.value == '就业困难'])        # 失业人员其中就业困难人数
 
-        hyhf_dic = Counter([i.value for i in all_ws['N'] if i.value and i.value in self.industries])
+        hyhf_dic = Counter([i.value for i, v in zip(all_ws['N'], all_ws['H']) if i.value and i.value in self.industries and isinstance(v.value, datetime) and v.value.month <= month])
         
         self.EP1.extend([xzjy_num, xzjy_num + zrjy_num, zrjy_num, syzjy_num, jykn_num])
         self.sjy01.extend([xzjy_num + zrjy_num, syzjy_num, jykn_num, xl_num, nv_num, cylb_dic.get('第一产业', 0), cylb_dic.get('第二产业', 0), cylb_dic.get('第三产业', 0),
@@ -80,26 +86,42 @@ class Write:
         # print(self.sy02)
         # print(self.hyhf)
     
-    def write(self):
-        bb_wb = load_workbook(self.bb_path)
+    def write(self, month : int, bb_wb):
+        row = month + 5
         for sheet, datas in [('人社统EP1', self.EP1), ('省就业01', self.sjy01), ('02城镇表登记失业人员情况', self.sy02), ('行业划分', self.hyhf)]:
             ws = bb_wb[sheet]
             for i, v in enumerate(datas, start=3):
-                ws.cell(row=self.row, column=i, value=v)
-        bb_wb.save(self.out_path)
-        print(f'文件{self.out_path}，写入完成')
+                ws.cell(row=row, column=i, value=v)
 
     def run(self, path, out_path):
         self.smz_path = path
         self.out_path = os.path.join(out_path, '就业统计报表.xlsx')
-        self.read()
-        self.write()
-        
+        smz_wb = load_workbook(self.smz_path)
+        all_ws = {i.value.month: '' for i in smz_wb['新就业人员']['H'] if i.value and isinstance(i.value, datetime)}
+        bb_wb = load_workbook(self.bb_path)
+        for month in list(all_ws.keys()):
+            self.re_init()
+            self.read(month)
+            self.write(month, bb_wb)
+        bb_wb.save(self.out_path)
+        print(f'文件{self.out_path}，写入完成')
+    
+    def main(self):
+        smz_wb = load_workbook(self.smz_path)
+        all_ws = {i.value.month: '' for i in smz_wb['新就业人员']['H'] if i.value and isinstance(i.value, datetime)}
+        bb_wb = load_workbook(self.bb_path)
+        for month in list(all_ws.keys()):
+            self.re_init()
+            self.read(month)
+            self.write(month, bb_wb)
+        bb_wb.save(self.out_path)
+        print(f'文件{self.out_path}，写入完成')
+
 
 if __name__ == '__main__':
     W = Write()
-    W.read()
-    W.write()
+    W.main()
+    
 
 
         
